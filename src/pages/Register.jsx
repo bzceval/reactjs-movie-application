5d80ec7097437bc3c2a9ba557e31787830d4be51
bzceval <busraaceval@gmail.com>
2022-11-03 23:46:19 +0300

    create state for register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Register = () => {
+  const [firstName, setFirstName] = useState()
+  const [lastName, setLastName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
   return (
-    <div>Register</div>
+    <div className='d-flex justify-content-center'>
+      <div className="form-image d-none d-md-block">
+        <img src={'https://picsum.photos/800/800'} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        
+        <h1 className="form-title display-3">Register</h1>
+        <form id="register">
+
+        <div className="mb-3">          
+          <label htmlFor="firstName" className='form-label'>First Name</label>
+          <input onChange={(e) => setFirstName(e.target.value)} type="text" className="form-control" id='firstName' placeholder='Enter Your First Name' required/>
+        </div>
+
+        <div className="mb-3"> 
+          <label htmlFor="lastName" className='form-label'>Last Name</label>
+          <input onChange={(e) => setLastName(e.target.value)} type="text" className="form-control" id='lastName' placeholder='Enter Your Last Name' required/>
+        </div>
+
+          <label htmlFor="email" className='form-label'>Email</label>
+          <input onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id='email' placeholder='Enter Your Email' required/>
+
+        <div className="mb-3"> 
+          <label htmlFor="password" className='form-label'>Password</label>
+          <input onChange={(e) => setPassword(e.target.value)} type="password" className="form-control" id='password' placeholder='Enter Your Password' required/>
+        </div>
+
+        <div className="mb-3">
+          <input type="submit" className='btn btn-primary form-control' value="Register" />
+        </div>
+
+        </form>
+
+      </div>
+    </div>
   )
 }
 
