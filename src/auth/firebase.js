e991d91a881e2a613c0ebb9d2ff454e9a683a653
bzceval <busraaceval@gmail.com>
2022-11-04 11:56:04 +0300

    create new user credential function

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -23,4 +23,32 @@ const auth = getAuth(app);
 
 
 //SIGN UP NEW USER
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
+// bu method yeni bir kullanıcı oluşturur.
+// 3 parametre alır authu zaten yukarıda oluşturduk
+// email ve passwordları da register inputlardan alarak parametrelerle göndereceğiz
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
+// bundan önce try catch bloğu olduğundan async bir methoddur. 
+// içerisinde bir fn oluşturacağuz ve email, password parametreleri göndereceğiz.
+// sonrada kullanmak istediğim yerde bu fni çağıracağız.
+// NOT başka yerde kullanmam için bu fni export etmeliyim
+export const createUser = async (email, password) => {
+  // yeni kullanıcı oluşturmak için firebase hazır methodu
+  try {
+    let userCredential = await createUserWithEmailAndPassword(auth, email, password)
+    console.log(userCredential)
+  } catch (error) {
+    console.log(error)
+  }
+}
